dca6a9df1e67fd65a0822e05a87d0bf6fa68b492
monalikarad5-png <karadmonali5@gmail.com>
2026-06-17 22:20:19 +0530

    javascript Miniproject updated

---
+++ Javascript_miniproject/Change-BG-Color-OnClick/little-demo.js
@@ -0,0 +1,26 @@
+// Events Little Demo practice
+
+console.log("hello");
+
+const mainButton = document.querySelector(".container button");
+// console.log(mainButton);
+const body = document.body;
+const currentColor = document.querySelector(".currentColor");
+
+function randomColorGenerator(){
+    console.log(Math.floor(Math.random()*10));
+    const red = Math.floor(Math.random()*256);
+    const blue = Math.floor(Math.random()*256);
+    const green = Math.floor(Math.random()*256);
+
+    const randomColor = `rgb(${red},${green},${blue})`;
+    return randomColor;
+}
+
+mainButton.addEventListener("click",(e)=>{
+    //    console.log("you clicked Me!!!");
+       const randomColor = randomColorGenerator();
+       console.log(randomColor);
+       body.style.backgroundColor = randomColor;
+       currentColor.textContent = randomColor;
+})
