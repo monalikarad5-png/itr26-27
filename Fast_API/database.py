9d94a79f2bf464b1ad30d476aa9b0beb0c371e93
monalikarad5-png <karadmonali5@gmail.com>
2026-07-01 20:51:56 +0530

    Fast API program updated

--- Fast_API/database.py
+++ Fast_API/database.py
@@ -1,1 +1,22 @@
-# in it we will create a database connection and a session to interact with the database using SQLAlchemy.
+from sqlalchemy import create_engine
+from sqlalchemy.orm import declarative_base, sessionmaker
+
+DATABASE_URL = "postgresql://postgres:admin%40123@localhost:5432/todo_db"
+
+engine = create_engine(DATABASE_URL)
+
+SessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine
+)
+
+Base = declarative_base()
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
